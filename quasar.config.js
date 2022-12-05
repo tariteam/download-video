de02b0baed58fa3ad2bab04b17ccc74256ab708b
Tachibana Shin <118260404+tachib-shin@users.noreply.github.com>
2022-12-05 15:55:34 +0000

    support codespaces preview

--- quasar.config.js
+++ quasar.config.js
@@ -10,6 +10,7 @@
 
 const path = require("path")
 
+const { extend } = require("quasar")
 const { configure } = require("quasar/wrappers")
 
 module.exports = configure(function (/* ctx */) {
@@ -71,7 +72,30 @@ module.exports = configure(function (/* ctx */) {
       // polyfillModulePreload: true,
       // distDir
 
-      // extendViteConf (viteConf) {},
+      extendViteConf(viteConf) {
+        extend(true, viteConf, {
+          server: {
+            // configure vite for HMR with Gitpod
+            hmr: process.env.GITPOD_WORKSPACE_URL
+              ? {
+                  // removes the protocol and replaces it with the port we're connecting to
+                  host: process.env.GITPOD_WORKSPACE_URL.replace(
+                    "https://",
+                    "9000-"
+                  ),
+                  protocol: "wss",
+                  clientPort: 443,
+                }
+              : process.env.CODESPACE_NAME
+              ? {
+                  host: `${process.env.CODESPACE_NAME}-9000.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`,
+                  protocol: "wss",
+                  clientPort: 443,
+                }
+              : true,
+          },
+        })
+      },
       // viteVuePluginOptions: {},
 
       vitePlugins: [
