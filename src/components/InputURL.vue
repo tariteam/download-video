9f68b6a754138a4551617fd89769d83102a1acaf
Tachibana Shin <118260404+tachib-shin@users.noreply.github.com>
2022-12-05 13:06:23 +0000

    setup clipboard

--- src/components/InputURL.vue
+++ src/components/InputURL.vue
@@ -5,13 +5,19 @@
       standout="text-white"
       class="input w-full max-w-[607px]"
       :placeholder="placeholder"
+      type="url"
       @keypress="emit('keypress:input')"
     >
       <template v-slot:prepend>
         <Icon icon="humbleicons:link" width="20" height="20" />
       </template>
       <template v-slot:append>
-        <q-btn no-caps unelevated class="bg-[rgba(25,118,210,0.2)] text-blue">
+        <q-btn
+          no-caps
+          unelevated
+          class="bg-[rgba(25,118,210,0.2)] text-blue"
+          @click="pasteFromClipboard"
+        >
           <Icon icon="fluent:clipboard-24-filled" />
           Dán
         </q-btn>
@@ -35,6 +41,7 @@
 
 <script lang="ts" setup>
 import { Icon } from "@iconify/vue"
+import { useQuasar } from "quasar";
 import { ref } from "vue"
 
 defineProps<{
@@ -45,8 +52,37 @@ const emit = defineEmits<{
   (name: "click:download", value: string): void
   (name: "keypress:input"): void
 }>()
+const $q = useQuasar()
 
 const url = ref("")
+
+async function pasteFromClipboard() {
+  try {
+    const permission = await navigator.permissions.query({
+      name: "clipboard-read",
+    })
+    if (permission.state === "denied") {
+      // eslint-disable-next-line functional/no-throw-statement
+      throw new Error("Not allowed to read clipboard.")
+    }
+    const clipboardContents = await navigator.clipboard.read()
+
+    for (const item of clipboardContents) {
+      if (!item.types.includes("text/plain")) {
+        // eslint-disable-next-line functional/no-throw-statement
+        throw new Error("Clipboard contains URL data.")
+      }
+      const blob = await item.getType("text/plain")
+
+      url.value = new TextDecoder().decode(await blob.arrayBuffer())
+    }
+  } catch (err) {
+    $q.notify({
+      position: "bottom-right",
+      message: (err as Error).message,
+    })
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
